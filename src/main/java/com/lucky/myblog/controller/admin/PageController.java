f27a467f1ca2f04ade2ac0dd7d49ad404fa7b493
zhangdd <86431843@qq.com>
2018-12-10 10:29:38 +0800

    feat:添加 删除页面
    Signed-off-by: zhangdd <86431843@qq.com>

--- src/main/java/com/lucky/myblog/controller/admin/PageController.java
+++ src/main/java/com/lucky/myblog/controller/admin/PageController.java
@@ -3,6 +3,7 @@ package com.lucky.myblog.controller.admin;
 import com.github.pagehelper.PageInfo;
 import com.lucky.myblog.constant.WebConst;
 import com.lucky.myblog.controller.BaseController;
+import com.lucky.myblog.dto.LogActions;
 import com.lucky.myblog.dto.Types;
 import com.lucky.myblog.model.bo.RestResponseBo;
 import com.lucky.myblog.model.vo.ContentVo;
@@ -75,6 +76,49 @@ public class PageController extends BaseController {
         }
         contents.setAuthorId(users.getUid());
         String result = contentsService.updateArticle(contents);
+        if (!WebConst.SUCCESS_RESULT.equals(result)) {
+            return RestResponseBo.fail(result);
+        }
+        return RestResponseBo.ok();
+    }
+
+    @RequestMapping(value = "delete")
+    @ResponseBody
+    public RestResponseBo delete(@RequestParam int cid, HttpServletRequest request) {
+        String result = contentsService.deleteByCid(cid);
+        logService.insertLog(LogActions.DEL_ARTICLE.getAction(), cid + "", request.getRemoteAddr(), this.getUid(request));
+        if (!WebConst.SUCCESS_RESULT.equals(result)) {
+            return RestResponseBo.fail(result);
+        }
+        return RestResponseBo.ok();
+    }
+
+    @GetMapping(value = "new")
+    public String newPage(HttpServletRequest request) {
+        return "admin/page_edit";
+    }
+
+    @PostMapping(value = "publish")
+    @ResponseBody
+    public RestResponseBo publishPage(@RequestParam String title, @RequestParam String content,
+                                      @RequestParam String status, @RequestParam String slug,
+                                      @RequestParam(required = false) Integer allowComment, @RequestParam(required = false) Integer allowPing, HttpServletRequest request) {
+
+        UserVo users = this.user(request);
+        ContentVo contents = new ContentVo();
+        contents.setTitle(title);
+        contents.setContent(content);
+        contents.setStatus(status);
+        contents.setSlug(slug);
+        contents.setType(Types.PAGE.getType());
+        if (null != allowComment) {
+            contents.setAllowComment(allowComment == 1);
+        }
+        if (null != allowPing) {
+            contents.setAllowPing(allowPing == 1);
+        }
+        contents.setAuthorId(users.getUid());
+        String result = contentsService.publish(contents);
         if (!WebConst.SUCCESS_RESULT.equals(result)) {
             return RestResponseBo.fail(result);
         }
